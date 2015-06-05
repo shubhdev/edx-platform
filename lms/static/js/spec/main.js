bfc123c802a8a154a481cb96f7be2b2ff37feba3
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-06-05 19:01:17 +0500

    Fixing moment.js error on jasmine specs.
    TNL-1958

--- lms/static/js/spec/main.js
+++ lms/static/js/spec/main.js
@@ -56,6 +56,7 @@
             'capa/display': 'xmodule_js/src/capa/display',
             'string_utils': 'xmodule_js/common_static/js/src/string_utils',
             'logger': 'xmodule_js/common_static/js/src/logger',
+            'moment': 'xmodule_js/common_static/js/vendor/moment-with-locales.min',
 
             // Manually specify LMS files that are not converted to RequireJS
             'history': 'js/vendor/history',
@@ -63,8 +64,7 @@
             'js/verify_student/photocapture': 'js/verify_student/photocapture',
             'js/staff_debug_actions': 'js/staff_debug_actions',
             'js/vendor/jquery.qubit': 'js/vendor/jquery.qubit',
-            // TODO fix the path to moment in order to run the tests.
-            //'moment': 'xmodule_js/common_static/js/vendor/moment-with-locales',
+
 
             // Backbone classes loaded explicitly until they are converted to use RequireJS
             'js/models/notification': 'js/models/notification',
